faa0f1c91bea53fbfc41fbee67648c3d2f8bd5c3
Abhay Gopinathan <abhay.gopinathan@mail.utoronto.ca>
2020-03-30 16:35:37 +0530

    Added capability to draw numbers and question mark in grid

--- src/drawing.c
+++ src/drawing.c
@@ -135,3 +135,65 @@ void clearGridLines(){
          drawLine(39,j,279,j,BLACK);
     }
 }
+
+void drawGridBox(int xpos, int ypos, char type){
+    //short int colour;
+    int* arr = NULL;
+    //int currentX,currentY,k;
+    int startX = 40 + 30*xpos;
+    int startY = 1 + 30*ypos;
+    if(type=='0'){
+        arr = zeros;
+    }
+    if(type == '1'){
+        arr = ones;
+    }
+    if(type == '2'){
+       arr = twos;
+    }
+    if(type=='3'){
+        arr = threes;
+    }
+    if(type=='4'){
+        arr = fours;
+    }
+    if(type=='q'){
+        arr = questions;
+    }
+    if(arr!=NULL){
+        for(int j=0; j<28; j++){
+            for(int i=0; i<28; i++){
+                int currentX = startX + i;
+                int currentY = startY + j;
+                int k = j*28 + i;
+                short int colour = arr[k];
+                plotPixel(currentX,currentY,colour);
+            
+            }
+        }
+    }
+}
+
+void convertArray(int* data, int* final){
+    for(int i=0; i<3136; i++){
+        if(i%2==0){
+            final[i/2] = (data[i] << 8) + (data[i+1]);
+        }
+    }
+}
+
+void clearGridBox(int xpos, int ypos){
+    int currentX,currentY;
+    int startX = 40 + 30*xpos;
+    int startY = 1 + 30*ypos;
+
+    for(int j=0; j<28; j++){
+        for(int i=0; i<28; i++){
+            currentX = startX + i;
+            currentY = startY + j;
+            plotPixel(currentX,currentY,BLACK);
+            
+        }
+    }
+
+}
